21b80a55f9bdc413624712f7e540521d6e7e026f
Nadini Annisa Byant <2111522021_nadini@student.unand.ac.id>
2025-05-16 00:21:21 +0700

    Progress Login dan SignUp

--- fe-yuklomba/src/App.jsx
+++ fe-yuklomba/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import NotFound from './NotFound';
 import TestComponent from './TestComponent';
+import LoginPage from './views/LoginPage';
+import SignUpPage from './views/SignUp';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path='*' element={<NotFound/>}/>
         <Route path='/test' element={<TestComponent/>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
       </Routes>
     </Router>
   );
